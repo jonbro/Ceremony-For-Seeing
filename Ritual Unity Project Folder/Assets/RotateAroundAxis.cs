6b881746ad4250b22de997cec1bda5a2d16f170e
jonbro <jonbro@gmail.com>
2016-01-29 20:29:17 -0800

    added the rotation stuff

---
+++ Ritual Unity Project Folder/Assets/RotateAroundAxis.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateAroundAxis : MonoBehaviour {
+	public Vector3 rotationAxis;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.rotation *= Quaternion.Euler(rotationAxis);
+	}
+}
