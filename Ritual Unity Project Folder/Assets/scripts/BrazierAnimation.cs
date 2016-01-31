cf5457cbddbc3dfdad2bed58bf1a2277cbebda53
jonbro <jonbro@gmail.com>
2016-01-31 13:54:22 -0800

    fixed up some of the stuff with new mesh

---
+++ Ritual Unity Project Folder/Assets/scripts/BrazierAnimation.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BrazierAnimation : MonoBehaviour {
+	public GameObject fireRitualPrefab;
+	void ObjectPlaced(GameObject obj){
+		GameObject fireRitual = (GameObject)Instantiate(fireRitualPrefab, obj.transform.position, Quaternion.identity);
+		fireRitual.transform.SetParent(obj.transform);
+		SendMessage("RitualComplete", obj);
+	}
+}
